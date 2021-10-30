9674602a589cd119eea6d2119c5637df45f46bed
xjb.laptop <xjbwks+laptop@gmail.com>
2021-10-30 16:11:35 +0900

    Add schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_14_094926) do
+ActiveRecord::Schema.define(version: 2021_10_14_095959) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "type"
+    t.text "description"
+    t.integer "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_books_on_book_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_10_14_094926) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "books", "books"
 end
