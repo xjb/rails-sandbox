504eda320e7f9b0ea7cdce4f834a7bf34d5c4fe2
xjb.laptop <xjbwks+laptop@gmail.com>
2021-11-07 17:43:10 +0900

    I18n :jp

---
+++ config/locales/ja.rb
@@ -0,0 +1,17 @@
+{
+  ja: {
+    i18n: {
+      plural: {
+        # keys: %i[zero one other],
+        keys: %i[one other],
+        rule: lambda do |n|
+          case n
+          # when 0 then :zero
+          when 1 then :one
+          else :other
+          end
+        end
+      }
+    }
+  }
+}
