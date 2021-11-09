3486486135c68697d08dc0c8face8c73419cd27d
xjb.laptop <xjbwks+laptop@gmail.com>
2021-11-09 21:35:37 +0900

    Add prefecture to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,6 +64,6 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :prefecture_id)
   end
 end
