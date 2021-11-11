94f7a679e355f6f150a4d1716a46d9c71da69e53
xjb.laptop <xjbwks+laptop@gmail.com>
2021-11-11 22:29:46 +0900

    FormBuilder for Bootstrap

--- app/helpers/bootstrap/form_builder.rb
+++ app/helpers/bootstrap/form_builder.rb
@@ -1,13 +1,190 @@
 module Bootstrap
+  # https://api.rubyonrails.org/v6.1.0/classes/ActionView/Helpers/FormBuilder.html
   class FormBuilder < ApplicationFormBuilder
-    # button                    date_select           field_helpers=             index         object          phone_field     telephone_field   to_partial_path
-    # check_box                 datetime_field        field_helpers?             label         object=         radio_button    text_area         url_field
-    # collection_check_boxes    datetime_local_field  fields                     month_field   object_name     range_field     text_field        week_field
-    # collection_radio_buttons  datetime_select       fields_for                 multipart     object_name=    rich_text_area  time_field
-    # collection_select         email_field           file_field                 multipart=    options         search_field    time_select
-    # color_field               emitted_hidden_id?    grouped_collection_select  multipart?    options=        select          time_zone_select
-    # date_field                field_helpers         hidden_field               number_field  password_field  submit          to_model
-    #
-    # enum_select
+    # https://github.com/rails/rails/blob/main/actionview/lib/action_view/helpers/form_helper.rb
+
+    def text_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def password_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def text_area(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def color_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control form-control-color'
+      super
+    end
+
+    def search_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def telephone_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def phone_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def date_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def time_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def datetime_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def datetime_local_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def month_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def week_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def url_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def email_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def number_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def range_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-range'
+      super
+    end
+
+    def fields_for(record_name, record_object = nil, fields_options = {}, &block)
+      super
+    end
+
+    def fields(scope = nil, model: nil, **options, &block)
+      super
+    end
+
+    def label(method, text = nil, options = {}, &block)
+      (options ||= {})[:class] ||= 'form-label'
+      super
+    end
+
+    def check_box(method, options = {}, checked_value = "1", unchecked_value = "0")
+      (options ||= {})[:class] ||= 'form-check-input'
+      super
+    end
+
+    def radio_button(method, tag_value, options = {})
+      (options ||= {})[:class] ||= 'form-check-input'
+      super
+    end
+
+    def hidden_field(method, options = {})
+      super
+    end
+
+    def file_field(method, options = {})
+      (options ||= {})[:class] ||= 'form-control'
+      super
+    end
+
+    def submit(value = nil, options = {})
+      value, options = nil, value if value.is_a?(Hash)
+      (options ||= {})[:class] ||= 'btn btn-dark'
+      super
+    end
+
+    def button(value = nil, options = {}, &block)
+      value, options = nil, value if value.is_a?(Hash)
+      (options ||= {})[:class] ||= 'btn btn-dark'
+      super
+    end
+
+    # https://github.com/rails/rails/blob/main/actionview/lib/action_view/helpers/form_options_helper.rb
+
+    def select(method, choices = nil, options = {}, html_options = {}, &block)
+      (html_options ||= {})[:class] ||= 'form-select'
+      super
+    end
+
+    def collection_select(method, collection, value_method, text_method, options = {}, html_options = {})
+      (html_options ||= {})[:class] ||= 'form-select'
+      super
+    end
+
+    def grouped_collection_select(method, collection, group_method, group_label_method, option_key_method, option_value_method, options = {}, html_options = {})
+      (html_options ||= {})[:class] ||= 'form-select'
+      super
+    end
+
+    def time_zone_select(method, priority_zones = nil, options = {}, html_options = {})
+      (html_options ||= {})[:class] ||= 'form-select'
+      super
+    end
+
+    # NOTE: BootstrapのForm gridは単一のlabel,inputのペアを<div class="form-check">でくくる必要があるため合わない
+    def collection_check_boxes(method, collection, value_method, text_method, options = {}, html_options = {}, &block)
+      (html_options ||= {})[:class] ||= 'form-check-input'
+      super
+    end
+
+    # NOTE: BootstrapのForm gridは単一のlabel,inputのペアを<div class="form-check">でくくる必要があるため合わない
+    def collection_radio_buttons(method, collection, value_method, text_method, options = {}, html_options = {}, &block)
+      (html_options ||= {})[:class] ||= 'form-check-input'
+      super
+    end
+
+    # https://github.com/rails/rails/blob/main/actionview/lib/action_view/helpers/date_helper.rb
+    # NOTE: BootstrapのForm gridは単一の要素を<div class="col">でくくる必要があるため合わない
+
+    def date_select(method, options = {}, html_options = {})
+      super
+    end
+
+    def time_select(method, options = {}, html_options = {})
+      super
+    end
+
+    def datetime_select(method, options = {}, html_options = {})
+      super
+    end
+
+    # https://github.com/rails/rails/blob/main/actiontext/app/helpers/action_text/tag_helper.rb
+
+    def rich_text_area(method, options = {})
+      super
+    end
   end
 end
