b9956f01be580bf3dbcf249c95ef1d831b5f875a
xjb.laptop <xjbwks+laptop@gmail.com>
2021-11-11 09:25:25 +0900

    Add enum_select

--- app/helpers/application_form_builder.rb
+++ app/helpers/application_form_builder.rb
@@ -1,2 +1,10 @@
 class ApplicationFormBuilder < ActionView::Helpers::FormBuilder
+  def enum_select(method, options = {}, html_options = {}, &block)
+    enum = @object.class.try!(method.to_s.pluralize)
+    choices = enum.keys.map do |key|
+      [I18n.t(key, scope: [:enum, @object_name, method], default: key), key]
+    end
+
+    select(method, choices, options, html_options, &block)
+  end
 end
--- app/helpers/bootstrap/form_builder.rb
+++ app/helpers/bootstrap/form_builder.rb
@@ -1,4 +1,13 @@
 module Bootstrap
   class FormBuilder < ApplicationFormBuilder
+    # button                    date_select           field_helpers=             index         object          phone_field     telephone_field   to_partial_path
+    # check_box                 datetime_field        field_helpers?             label         object=         radio_button    text_area         url_field
+    # collection_check_boxes    datetime_local_field  fields                     month_field   object_name     range_field     text_field        week_field
+    # collection_radio_buttons  datetime_select       fields_for                 multipart     object_name=    rich_text_area  time_field
+    # collection_select         email_field           file_field                 multipart=    options         search_field    time_select
+    # color_field               emitted_hidden_id?    grouped_collection_select  multipart?    options=        select          time_zone_select
+    # date_field                field_helpers         hidden_field               number_field  password_field  submit          to_model
+    #
+    # enum_select
   end
 end
