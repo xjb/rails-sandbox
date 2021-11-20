4517946c9077e301470abb79c66a722c8ba4cb5c
xjb.laptop <xjbwks+laptop@gmail.com>
2021-11-20 20:25:19 +0900

    specを追加

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  # delegate :sign_up, to: :UserMailer
 
+  def sign_up
+    UserMailer.sign_up
+  end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,13 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#sign_up' do
+    subject(:mail) { described_class.sign_up }
+
+    it { expect(mail.to).to eq ['notifications@example.com'] }
+    it { expect(mail.from).to eq ['from@example.com'] }
+    it { expect(mail.subject).to eq '[UserMailer] sign_up' }
+    it { expect(mail.html_part.body).to match /html/ }
+    it { expect(mail.text_part.body).to match /text/ }
+  end
 end
