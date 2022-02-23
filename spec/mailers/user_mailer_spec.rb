45cd041e112de32fdaff5876013c88a21e14de56
xjb.laptop <xjbwks+laptop@gmail.com>
2022-02-23 13:30:41 +0900

    fix

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe UserMailer, type: :mailer do
     it { expect(mail.to).to eq ['notifications@example.com'] }
     it { expect(mail.from).to eq ['from@example.com'] }
     it { expect(mail.subject).to eq '[UserMailer] sign_up' }
-    it { expect(mail.html_part.body).to match /html/ }
-    it { expect(mail.text_part.body).to match /text/ }
+    it { expect(mail.html_part.body).to match(/html/) }
+    it { expect(mail.text_part.body).to match(/text/) }
   end
 end
